e160fcf651ae87414d45326b8b7b9eb6444032c4
alexandrekloch <alexandrekloch@windowslive.com>
2013-09-19 13:20:05 -0300

    CRUD
    App desenvovido para apresentar técnicas básicas de CRUD

---
+++ CRUDWindowsForms/CRUDWindowsForms/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CRUDWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstContatos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtPesquisar = new System.Windows.Forms.TextBox();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader4});
+            this.lstContatos.Location = new System.Drawing.Point(12, 82);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(284, 174);
+            this.lstContatos.TabIndex = 0;
+            this.lstContatos.UseCompatibleStateImageBehavior = false;
+            this.lstContatos.View = System.Windows.Forms.View.Details;
+            this.lstContatos.SelectedIndexChanged += new System.EventHandler(this.lstContatos_SelectedIndexChanged);
+            this.lstContatos.Click += new System.EventHandler(this.lstContatos_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nome";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Id";
+            // 
+            // txtPesquisar
+            // 
+            this.txtPesquisar.Location = new System.Drawing.Point(12, 49);
+            this.txtPesquisar.Name = "txtPesquisar";
+            this.txtPesquisar.Size = new System.Drawing.Size(193, 20);
+            this.txtPesquisar.TabIndex = 1;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(212, 49);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 2;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Pesquisar Nome";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFechar);
+            this.groupBox1.Controls.Add(this.btnDeletar);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.btnSalvar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Location = new System.Drawing.Point(313, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 174);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(159, 143);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 19;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(77, 143);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btnDeletar.TabIndex = 18;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(159, 113);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 17;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(77, 113);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 16;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(58, 66);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(193, 20);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Nome";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(58, 40);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(193, 20);
+            this.txtNome.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 264);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.txtPesquisar);
+            this.Controls.Add(this.lstContatos);
+            this.Name = "Form1";
+            this.Text = "Contatos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstContatos;
+        private System.Windows.Forms.TextBox txtPesquisar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ CRUDWindowsForms/CRUDWindowsForms/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        String status = "novo";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DataTable SelecionaContatos()
+        {
+
+            SqlConnection conn = new SqlConnection();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+
+            try
+            {
+                conn.ConnectionString = Properties.Settings.Default.csBDContatos;
+                da.SelectCommand = new SqlCommand();
+                da.SelectCommand.CommandText = "SELECT * FROM tblContatos";
+                da.SelectCommand.Connection = conn;
+                da.SelectCommand.CommandType = CommandType.Text;
+
+                da.Fill(dt);
+
+                return dt;
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception("Sql Exception " + ex.Number);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtualizaLista();
+        }
+
+        private void AtualizaLista()
+        {
+            lstContatos.Items.Clear();
+            DataTable contatos = SelecionaContatos();
+            foreach (DataRow contato in contatos.Rows)
+            {
+                ListViewItem list = new ListViewItem(contato["id"].ToString());
+                list.SubItems.Add(contato["Nome"].ToString());
+                list.SubItems.Add(contato["Email"].ToString());
+                lstContatos.Items.Add(list);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if ((txtNome.Text != "") && (txtEmail.Text != ""))
+            {
+                if (status == "novo")
+                {
+                   CriarContato(txtNome.Text, txtEmail.Text);
+                   LimparFormulario();
+                   AtualizaLista();
+                }
+                else if (status == "editar")
+                {
+                    int num = Convert.ToInt32(lstContatos.Items[lstContatos.FocusedItem.Index].Text);
+                    AtulizaContato(num, txtNome.Text, txtEmail.Text);
+                    LimparFormulario();
+                    AtualizaLista();
+                    status = "novo";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Preencha o campo!!!");
+            }
+        }
+
+        private void AtulizaContato(int num, string nome, string email)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand da = new SqlCommand();
+
+            try
+            {
+                conn.ConnectionString = Properties.Settings.Default.csBDContatos;
+                da.CommandText = "Update tblContatos set nome '" + nome +
+                                                     "', email = '" + email + 
+                                                     "' where id = " + num;
+                da.Connection = conn;
+                da.CommandType = CommandType.Text;
+                conn.Open();
+                da.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Sql Exception " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Contato Deletado com sucesso!!");
+            }
+        }
+
+        private void LimparFormulario()
+        {
+            txtEmail.Clear();
+            txtNome.Clear();
+        }
+
+        private void CriarContato(string nome, string email)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand da = new SqlCommand();
+            
+            try
+            {
+                conn.ConnectionString = Properties.Settings.Default.csBDContatos;
+                da.CommandText = "Insert INTO tblContatos(Nome,Email) Values( '" + nome + "','" + email + "')";
+                da.Connection = conn;
+                da.CommandType = CommandType.Text;
+                conn.Open();
+                da.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Sql Exception " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Contato cadastrado com sucesso!!");
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparFormulario();
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(lstContatos.Items[lstContatos.FocusedItem.Index].Text);
+            
+            DeletarContato(num);
+            AtualizaLista();
+        }
+
+        private void DeletarContato(int id)
+        {
+            SqlConnection conn = new SqlConnection();
+            SqlCommand da = new SqlCommand();
+
+            try
+            {
+                conn.ConnectionString = Properties.Settings.Default.csBDContatos;
+                da.CommandText = "Delete FROM tblContatos where id =" + id;
+                da.Connection = conn;
+                da.CommandType = CommandType.Text;
+                conn.Open();
+                da.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Sql Exception " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+                MessageBox.Show("Contato Deletado com sucesso!!");
+            }
+        }
+
+        private void lstContatos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstContatos_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = lstContatos.Items[lstContatos.FocusedItem.Index].SubItems[1].Text; 
+            txtEmail.Text = lstContatos.Items[lstContatos.FocusedItem.Index].SubItems[2].Text;
+            status = "editar";
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
