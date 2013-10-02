ddc072326498b7c905256e45aa00705b92f1c8bf
alexandrekloch <alexandrekloch@windowslive.com>
2013-10-02 14:17:48 -0300

    Construtores 1
    Construtores parte 1

---
+++ AtributosMetodosEstaticos 1/AtributosMetodosEstaticos/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AtributosMetodosEstaticos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pessoas p1 = new Pessoas();
+            p1.Nome = "Alexandre";
+            
+
+            listBox1.Items.Add(p1.Nome);
+            listBox1.Items.Add(Pessoas.ContadorPessoas.ToString());
+
+            Pessoas p2 = new Pessoas();
+            p2.Nome = "Eduardo";
+            
+
+            listBox1.Items.Add(p2.Nome);
+            listBox1.Items.Add(Pessoas.ContadorPessoas.ToString());
+
+
+
+
+        }
+    }
+}
---
+++ AtributosMetodosEstaticos 1/AtributosMetodosEstaticos/Pessoas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AtributosMetodosEstaticos
+{
+    class Pessoas
+    {
+        private String nome;
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private static int contadorPessoas = 0;
+        public static int ContadorPessoas
+        {
+            get { return Pessoas.contadorPessoas; }
+            set { Pessoas.contadorPessoas = value; }
+        }
+
+        public Pessoas()
+        {
+            ContadorPessoas++;
+        }
+
+
+
+
+    }
+}
