f581786cdb30ffb6ba3288ceb075401748e59a06
alexandrekloch <alexandrekloch@windowslive.com>
2013-11-04 16:58:53 -0200

    Uso de List

---
+++ ClassesWeb/ClassesWeb/CalendarioDeAniversarios.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClassesWeb
+{
+    public partial class CalendarioDeAniversarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            List<Pessoa> lst = new List<Pessoa>();
+            lst = Pessoa.ListarAniverssariantes(new DateTime(1977,07,20));
+
+            foreach (var item in lst)
+            {
+                Literal l = new Literal();
+                l.Text = item.nome + "<br/>";
+                form1.Controls.Add(l);
+            }
+        }
+    }
+}
---
+++ ClassesWeb/ClassesWeb/Pessoas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassesWeb
+{
+    public class Pessoa
+    {
+        public String nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public String redeSocial { get; set; }
+
+        public Pessoa()
+        {
+
+        }
+
+        public Pessoa(String nome, DateTime dataNascimento, String redeSocial) 
+        {
+            this.nome = nome;
+            this.dataNascimento = dataNascimento;
+            this.redeSocial = redeSocial;
+        }
+
+        public static List<Pessoa> ListarAniverssariantes(DateTime data)
+        {
+            List<Pessoa> lista = new List<Pessoa>();
+            
+            lista.Add(new Pessoa("Alexandre Luis Kloch", new DateTime(1977, 12,20),"https://www.facebook.com/alexandre.kloch"));
+            lista.Add(new Pessoa("Karine", new DateTime(1980, 07, 20), "https://www.facebook.com/alexandre.kloch"));
+            lista.Add(new Pessoa("Eduardo", new DateTime(2011, 04, 11), "https://www.facebook.com/alexandre.kloch"));
+            lista.Add(new Pessoa("Juraci", new DateTime(1939, 07, 18), "https://www.facebook.com/alexandre.kloch"));
+            
+
+            List<Pessoa> aniversarianteMes = lista.FindAll(
+                delegate(Pessoa p1) 
+                {
+                    return p1.dataNascimento.Date.Month == data.Month ; 
+                });
+
+            //List<Pessoa> aniversariantedia = aniversarianteMes.FindAll(
+            //    delegate(Pessoa p1)
+            //    {
+            //        return p1.dataNascimento.Date.Day == data.Day;
+            //    });
+            return aniversarianteMes;
+
+
+ 
+        }
+    }
+}
